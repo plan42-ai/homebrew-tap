b5e0fb39ec471a10640371c3311d935db5810c0d
scottwis <scottwis@users.noreply.github.com>
2026-07-06 16:48:14 +0000

    Update plan42 formula to v1.0.59

--- Formula/plan42.rb
+++ Formula/plan42.rb
@@ -1,7 +1,7 @@
 class Plan42 < Formula
   desc "CLI for Plan42.ai"
   homepage "https://github.com/plan42-ai/cli"
-  version "1.0.58"
+  version "1.0.59"
 
   depends_on arch: :arm64
   depends_on macos: :tahoe
@@ -9,7 +9,7 @@ class Plan42 < Formula
   on_macos do
     on_arm do
       url "https://github.com/plan42-ai/cli/releases/download/v#{version}/plan42-#{version}-macos-arm64.tgz"
-      sha256 "86f15a93da6f5d25a5388490e054f3b524f70b615c8c0a8ce08899decc67f0d1"
+      sha256 "e307f74a0ce93bf737398cda3c9667dfaf3c4dcef2bde1986c776b4b2fc7db74"
     end
   end
 
