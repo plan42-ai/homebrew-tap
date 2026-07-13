55fe188df677eb930fa559031d7cdd70ce4cbabd
Scott Wisniewski <scott@agile.security>
2026-07-13 14:22:31 -0700

    Update plan42 to 1.0.64.

--- Formula/plan42.rb
+++ Formula/plan42.rb
@@ -1,7 +1,7 @@
 class Plan42 < Formula
   desc "CLI for Plan42.ai"
   homepage "https://github.com/plan42-ai/cli"
-  version "1.0.63"
+  version "1.0.64"
 
   depends_on arch: :arm64
   depends_on macos: :tahoe
@@ -9,7 +9,7 @@ class Plan42 < Formula
   on_macos do
     on_arm do
       url "https://github.com/plan42-ai/cli/releases/download/v#{version}/plan42-#{version}-macos-arm64.tgz"
-      sha256 "72589ffa7c1f5b34dfb6b31af38c110eb172f01139772fcbd1383930b60a87ba"
+      sha256 "9a097123ecdeb520fbc87142456ae9c4c4159762a6ccd184adb50248760c8941"
     end
   end
 
